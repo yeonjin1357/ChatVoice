3276fb616d546fbfde8729c82e8509b1b3ee8d26
yeonjin1357 <yeonjin1357@naver.com>
2024-02-14 00:36:38 +0900

    ㄴㄴ

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import classes from "./ChatInterface.module.css";
 import OpenAI from "openai";
 
