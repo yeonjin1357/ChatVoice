17fe46ba58477dda4287cc40318aea03df418e03
yeonjin1357 <yeonjin1357@naver.com>
2024-02-20 17:35:41 +0900

    select 수정

--- src/components/ChatbotSelection.jsx
+++ src/components/ChatbotSelection.jsx
@@ -1,7 +1,15 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import classes from "./ChatbotSelection.module.css"; // 스타일을 위한 CSS 모듈
+// Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+import classes from "./ChatbotSelection.module.css";
 
 const ChatbotSelection = () => {
   const chatbots = useSelector((state) => state.chatbot.chatbots);
@@ -9,16 +17,35 @@ const ChatbotSelection = () => {
 
   return (
     <div className={classes.container}>
-      {chatbots.map((bot) => (
-        <div className={classes.card} key={bot.id} onClick={() => navigate(`/${bot.id}`)}>
-          <img src={bot.image} alt={bot.name} className={classes.image} />
-          <div className={classes.info}>
-            <h2>{bot.name}</h2>
-            <p>{bot.description}</p>
-            <button className={classes.button}>채팅 참여하기</button>
-          </div>
-        </div>
-      ))}
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className={classes.swiper}
+      >
+        {chatbots.map((bot) => (
+          <SwiperSlide key={bot.id} onClick={() => navigate(`/${bot.id}`)}>
+            <div className={classes.card}>
+              <img src={bot.image} alt={bot.name} className={classes.image} />
+              <div className={classes.info}>
+                <h2>{bot.name}</h2>
+                <p>{bot.description}</p>
+                <button className={classes.button}>채팅 참여하기</button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
